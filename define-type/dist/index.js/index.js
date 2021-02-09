bc1454ed45d74cf75f5e8aec45402456a0dc8c4b
unknown <cheonyulin@gmail.com>
2021-02-10 00:45:01 +0900

    TS for JS Programmer(타입 스크립트 핸드북 사이트) 보면서, 타입 선언에 대한 공부

--- define-type/dist/index.js/index.js
+++ define-type/dist/index.js/index.js
@@ -1,2 +1,10 @@
-console.log("hello world");
+class UserAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const getUserInfo = (user) => `Hello, my name is ${user.name}`;
+const user = new UserAccount("Mason Yun", 0);
+console.log(getUserInfo(user));
 //# sourceMappingURL=index.js.map
--- define-type/src/index.ts
+++ define-type/src/index.ts
@@ -1,1 +1,35 @@
-console.log("hello world");
+
+/* name: string과 id:number을 포함하는 추론 타입을 가진 객체를 생성하는 예제 */
+interface User{
+    name: string;
+    id: number;
+}
+
+/* 새로운 객체가 인터페이스 형태를 따르고 있다. */
+/*  객체 선언 형태
+const user:User = {
+    name: "Mason Yun",
+    id: 0,
+//  nickname: "mason" => 형식에 맞지 않으므로 경고를 준다.
+}
+*/
+
+/* 클래스로도 선언할 수 있다. */
+class UserAccount {
+    name: string;
+    id: number;
+
+    constructor(name: string, id:number){
+        this.name = name;
+        this.id = id;
+    }
+}
+
+const getUserInfo = (user: User):string => `Hello, my name is ${user.name}`;
+
+const user:User = new UserAccount("Mason Yun", 0);
+
+console.log(getUserInfo(user));
+
+
+
